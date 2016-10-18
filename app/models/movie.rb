ae4ff4c9a9a001cef7a6c7822ea6df3985fcc8af
Digveer <digveer@outlook.com>
2016-10-18 18:08:17 -0500

    Delete.
    Deleted .

--- app/models/movie.rb
+++
@@ -1,8 +0,0 @@
-class Movie < ActiveRecord::Base
-
-    def self.all_ratings
-        rating_array=Array.new
-        self.select("rating").uniq.each{|x| rating_array.push(x.rating)}
-        rating_array.sort.uniq
-    end
-end
